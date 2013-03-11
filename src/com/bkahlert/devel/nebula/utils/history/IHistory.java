73a4529ef51f677b2b1e00ba245aa36da1f0cefe
Björn Kahlert <mail@bkahlert.com>
2013-03-12 00:37:33 +0100

    Added browser like history functionality

---
+++ src/com/bkahlert/devel/nebula/utils/history/IHistory.java
@@ -0,0 +1,78 @@
+package com.bkahlert.devel.nebula.utils.history;
+
+import com.bkahlert.devel.nebula.utils.history.History.NoNextElementException;
+import com.bkahlert.devel.nebula.utils.history.History.NoPreviousElementException;
+
+/**
+ * This class provides the functionality needed to implement a browser like
+ * history.
+ * 
+ * @author bkahlert
+ * 
+ * @param <T>
+ *            type this {@link IHistory} can hold
+ */
+public interface IHistory<T> {
+
+	/**
+	 * Returns the current position in this {@link History}.
+	 * 
+	 * @return -1 if empty
+	 */
+	public int pos();
+
+	/**
+	 * Returns the number of elements in this {@link History}.
+	 * 
+	 * @return
+	 */
+	public int size();
+
+	/**
+	 * Returns true if you can navigate one step backwards.
+	 * 
+	 * @return
+	 */
+	public boolean hasPrev();
+
+	/**
+	 * Returns true if you can navigate one step forward.
+	 * 
+	 * @return
+	 */
+	public boolean hasNext();
+
+	/**
+	 * Returns the currently active element in this {@link History}.
+	 * 
+	 * @return null if empty
+	 */
+	public T get();
+
+	/**
+	 * Adds one element to this {@link History} and makes it the currently
+	 * active element.
+	 * 
+	 * @param element
+	 */
+	public void add(T element);
+
+	/**
+	 * Moves one element back in this {@link History} and returns the newly
+	 * active element.
+	 * 
+	 * @return
+	 * @throws NoPreviousElementException
+	 */
+	public T back() throws NoPreviousElementException;
+
+	/**
+	 * Moves one element forward in this {@link History} and returns the newly
+	 * active element.
+	 * 
+	 * @return
+	 * @throws NoNextElementException
+	 */
+	public T forward() throws NoNextElementException;
+
+}
