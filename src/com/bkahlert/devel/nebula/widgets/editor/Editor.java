e1bf403ded8a295fffdcaed707d858e0265ca92d
Björn Kahlert <mail@bkahlert.com>
2013-03-30 03:48:57 +0100

    .

--- src/com/bkahlert/devel/nebula/widgets/editor/Editor.java
+++ src/com/bkahlert/devel/nebula/widgets/editor/Editor.java
@@ -16,6 +16,7 @@ import org.eclipse.swt.SWT;
 import org.eclipse.swt.graphics.Color;
 import org.eclipse.swt.layout.FillLayout;
 import org.eclipse.swt.widgets.Composite;
+import org.eclipse.swt.widgets.Display;
 import org.eclipse.swt.widgets.Listener;
 
 import com.bkahlert.devel.nebula.utils.EventDelegator;
@@ -269,10 +270,12 @@ public abstract class Editor<T> extends Composite {
 					e);
 		}
 
-		for (Editor<Object> responsibleEditor : responsibleEditors
-				.get(this.loadedObject)) {
-			if (responsibleEditor != this) {
-				responsibleEditor.composer.setSource(html);
+		if (responsibleEditors.get(this.loadedObject) != null) {
+			for (Editor<Object> responsibleEditor : responsibleEditors
+					.get(this.loadedObject)) {
+				if (responsibleEditor != this) {
+					responsibleEditor.composer.setSource(html);
+				}
 			}
 		}
 
