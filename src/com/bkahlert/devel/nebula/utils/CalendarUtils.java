8b1cbd25751bbcb1e51c0e76cfe6737e4730399f
Björn Kahlert <mail@bkahlert.com>
2014-02-12 06:04:03 +0100

    [INTERNAL] Improved CalendarUtils - Rewritten parsing functionality

--- src/com/bkahlert/devel/nebula/utils/CalendarUtils.java
+++ src/com/bkahlert/devel/nebula/utils/CalendarUtils.java
@@ -1,34 +1,33 @@
 package com.bkahlert.devel.nebula.utils;
 
+import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.util.Arrays;
 import java.util.Calendar;
 import java.util.List;
 import java.util.TimeZone;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
-import javax.xml.datatype.DatatypeConfigurationException;
-import javax.xml.datatype.DatatypeFactory;
-
+import org.apache.commons.lang.StringUtils;
 import org.apache.log4j.Logger;
 
 public class CalendarUtils {
 
+	@SuppressWarnings("unused")
 	private static final Logger LOGGER = Logger.getLogger(CalendarUtils.class);
 
-	private static DatatypeFactory datatypeFactory = null;
+	// Input
+	public static final String DATE_PATTERN = "([0-9]+)[^\\d]([0-9]+)[^\\d]([0-9]+)";
+	public static final String TIME_PATTERN = DATE_PATTERN + "(\\.([0-9]+))?";
+	public static final String TIMEZONE_PATTERN = "(([+-])(([0-9]+)[^\\d]([0-9]+)|([0-9]{1,2})([0-9]{2})))?";
+	public static final Pattern FROM_ISO8601 = Pattern.compile(DATE_PATTERN
+			+ "T" + TIME_PATTERN + TIMEZONE_PATTERN);
+	public static final int FRACTION_LENGTH = 3;
 
-	private static DatatypeFactory getDatatypeFactory() {
-		if (datatypeFactory == null) {
-			try {
-				datatypeFactory = DatatypeFactory.newInstance();
-			} catch (DatatypeConfigurationException e) {
-				LOGGER.fatal(
-						"Error creating "
-								+ DatatypeFactory.class.getSimpleName(), e);
-			}
-		}
-		return datatypeFactory;
-	}
+	// Output
+	public static final String ISO8601 = "yyyy-MM-dd'T'HH:mm:ss.SSSZ";
+	public static final String ISO8601_FILESYSTEM = "yyyy-MM-dd'T'HH-mm-ss.SSSZ";
 
 	private static Calendar[] getSortedCalendars(List<Calendar> calendars) {
 		Calendar[] calendarArray = calendars.toArray(new Calendar[0]);
@@ -72,8 +71,7 @@ public class CalendarUtils {
 	 * @return
 	 */
 	public static String toISO8601(Calendar calendar) {
-		SimpleDateFormat iso8601 = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH:mm:ss.SZ");
+		SimpleDateFormat iso8601 = new SimpleDateFormat(ISO8601);
 		iso8601.setTimeZone(calendar.getTimeZone());
 		String missingDots = iso8601.format(calendar.getTime()).replace("GMT",
 				"");
@@ -82,8 +80,7 @@ public class CalendarUtils {
 	}
 
 	public static String toISO8601FileSystemCompatible(Calendar calendar) {
-		SimpleDateFormat iso8601 = new SimpleDateFormat(
-				"yyyy-MM-dd'T'HH-mm-ss.SZ");
+		SimpleDateFormat iso8601 = new SimpleDateFormat(ISO8601_FILESYSTEM);
 		iso8601.setTimeZone(calendar.getTimeZone());
 		String missingDots = iso8601.format(calendar.getTime()).replace("GMT",
 				"");
@@ -93,43 +90,139 @@ public class CalendarUtils {
 
 	public static Calendar fromISO8601(String lexicalRepresentation)
 			throws IllegalArgumentException {
-		String[] tries = new String[] { lexicalRepresentation,
-				cleanISO8601(lexicalRepresentation) };
+		Matcher matcher = FROM_ISO8601.matcher(lexicalRepresentation);
+		if (matcher.find()) {
+			String year = matcher.group(1);
+			String month = matcher.group(2);
+			String date = matcher.group(3);
+			String hour = matcher.group(4);
+			String minute = matcher.group(5);
+			String second = matcher.group(6);
+			String fraction = StringUtils.rightPad(
+					matcher.group(8) != null ? matcher.group(8) : "",
+					FRACTION_LENGTH, "0");
 
-		IllegalArgumentException ex = null;
-		for (String trie : tries) {
-			try {
-				return getDatatypeFactory().newXMLGregorianCalendar(trie)
-						.toGregorianCalendar();
-			} catch (IllegalArgumentException e) {
-				if (ex == null) {
-					ex = e;
-				}
+			int year_ = Integer.valueOf(year);
+			int month_ = Integer.valueOf(month) - 1;
+			int date_ = Integer.valueOf(date);
+			int hour_ = Integer.valueOf(hour);
+			int minute_ = Integer.valueOf(minute);
+			int second_ = Integer.valueOf(second);
+			int fraction_ = (int) Math.round(Double.valueOf(fraction.substring(
+					0, FRACTION_LENGTH)
+					+ "."
+					+ fraction.substring(FRACTION_LENGTH)));
+
+			Calendar calendar = Calendar.getInstance();
+			calendar.set(Calendar.YEAR, year_);
+			calendar.set(Calendar.MONTH, month_);
+			calendar.set(Calendar.DATE, date_);
+			calendar.set(Calendar.HOUR_OF_DAY, hour_);
+			calendar.set(Calendar.MINUTE, minute_);
+			calendar.set(Calendar.SECOND, second_);
+			calendar.set(Calendar.MILLISECOND, fraction_);
+
+			String timezoneSign;
+			Integer timezoneHour_;
+			Integer timezoneMinute_;
+			if (matcher.group(9) != null) {
+				timezoneSign = matcher.group(10);
+				String timezoneHour = matcher.group(12) != null ? matcher
+						.group(12) : matcher.group(14);
+				String timezoneMinute = matcher.group(13) != null ? matcher
+						.group(13) : matcher.group(15);
+
+				timezoneHour_ = Integer.valueOf(timezoneHour) % 24;
+				timezoneMinute_ = Integer.valueOf(timezoneMinute) % 60;
+			} else {
+				int offset = TimeZone.getDefault().getOffset(
+						calendar.getTimeInMillis());
+				calendar.add(Calendar.MILLISECOND, offset);
+
+				timezoneSign = offset >= 0 ? "+" : "-";
+				/*
+				 * Double x = offset / 3600000.0; String[] parts =
+				 * x.toString().split("\\.");
+				 * 
+				 * timezoneHour_ = Integer.valueOf(parts[0]); timezoneMinute_ =
+				 * (int) (Double.valueOf("." + parts[1]) * 60);
+				 */
+				timezoneHour_ = timezoneMinute_ = 0;
 			}
+
+			TimeZone timeZone = TimeZone.getTimeZone("GMT" + timezoneSign
+					+ StringUtils.leftPad(timezoneHour_.toString(), 2, "0")
+					+ ":"
+					+ StringUtils.leftPad(timezoneMinute_.toString(), 2, "0"));
+			calendar.setTimeZone(timeZone);
+
+			return calendar;
 		}
-		throw ex;
+		throw new IllegalArgumentException("Could not parse "
+				+ lexicalRepresentation);
 	}
 
-	public static String cleanISO8601(String lexical) {
-		// replace dashes in time by colons
-		String[] parts = lexical.split("T");
-		if (parts.length != 2) {
-			return lexical;
+	public static String toShort(Calendar calendar) {
+		return DateFormat.getDateTimeInstance(DateFormat.MEDIUM,
+				DateFormat.SHORT).format(calendar.getTime());
+	}
+
+	/**
+	 * Formats the given {@link Calendar} while considering the time zone
+	 * information.
+	 * 
+	 * @param calendar
+	 * @param pattern
+	 * @return
+	 */
+	public static String format(Calendar calendar, String pattern) {
+		return format(calendar, new SimpleDateFormat(pattern));
+	}
+
+	/**
+	 * Formats the given {@link Calendar} while considering the time zone
+	 * information.
+	 * 
+	 * @param calendar
+	 * @param dateFormat
+	 * @return
+	 */
+	public static String format(Calendar calendar, DateFormat dateFormat) {
+		dateFormat.setTimeZone(calendar.getTimeZone());
+		return dateFormat.format(calendar.getTime());
+	}
+
+	public static int compareToTimeZoneLess(Calendar calendar1,
+			Calendar calendar2) {
+		long localTime = CalendarUtils.getLocalTime(calendar1);
+		long otherLocalTime = CalendarUtils.getLocalTime(calendar2);
+		return new Long(localTime).compareTo(otherLocalTime);
+	}
+
+	public static boolean before(Calendar calendar1, Calendar calendar2) {
+		return calendar2.compareTo(calendar1) > 0;
+	}
+
+	public static boolean after(Calendar calendar1, Calendar calendar2) {
+		return calendar2.compareTo(calendar1) < 0;
+	}
+
+	/**
+	 * Return the milliseconds passed since 1.1.1970 00:00:00.000 [TimeZone]
+	 * 
+	 * @param calendar
+	 * @return
+	 */
+	public static long getLocalTime(Calendar calendar) {
+		return calendar.getTimeInMillis()
+				+ calendar.getTimeZone().getOffset(calendar.getTimeInMillis());
+	}
+
+	public static Calendar addMilliseconds(Calendar calendar, Long amount) {
+		if (amount != null) {
+			calendar.add(Calendar.MILLISECOND, (int) amount.floatValue());
 		}
-
-		parts[1] = parts[1].substring(0, 2) + ":" + parts[1].substring(3, 5)
-				+ ":" + parts[1].substring(6);
-		lexical = parts[0] + "T" + parts[1];
-
-		// insert colon in timezone
-		String timeZoneVorzeichen = lexical.substring(lexical.length() - 5,
-				lexical.length() - 4);
-		if (timeZoneVorzeichen.equals("+") || timeZoneVorzeichen.equals("-")) {
-			lexical = lexical.substring(0, lexical.length() - 2) + ":"
-					+ lexical.substring(lexical.length() - 2);
-		}
-
-		return lexical;
+		return calendar;
 	}
 
 }
--- test/com/bkahlert/devel/nebula/utils/CalendarUtilsTest.java
+++ test/com/bkahlert/devel/nebula/utils/CalendarUtilsTest.java
@@ -1,17 +1,22 @@
 package com.bkahlert.devel.nebula.utils;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import java.text.ParseException;
 import java.util.Arrays;
 import java.util.Calendar;
+import java.util.Date;
+import java.util.TimeZone;
 
+import org.junit.Assert;
 import org.junit.Test;
 
 public class CalendarUtilsTest {
 
-	private Calendar cal1;
-	private Calendar cal2;
-	private Calendar cal3;
+	private final Calendar cal1;
+	private final Calendar cal2;
+	private final Calendar cal3;
 
 	public CalendarUtilsTest() {
 		this.cal1 = Calendar.getInstance();
@@ -25,32 +30,235 @@ public class CalendarUtilsTest {
 	}
 
 	@Test
+	public void defaultConstructor() {
+		Assert.assertTrue(Math.abs(new Date().getTime()
+				- Calendar.getInstance().getTimeInMillis()) < 10);
+	}
+
+	@Test
+	public void testToFromISO8601() throws Exception {
+		// complete
+		assertEquals("1984-05-15T14:30:04.123+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.123+01:30")));
+		assertEquals("1984-05-15T14:30:04.123-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.123-02:45")));
+
+		// rounding
+		assertEquals("1999-12-31T23:59:59.999+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1999-12-31T23:59:59.9994+01:30")));
+		assertEquals("2000-01-01T00:00:00.000-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1999-12-31T23:59:59.9995-02:45")));
+
+		// too many characters in milliseconds
+		assertEquals("1984-05-15T14:30:04.123+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.1234+01:30")));
+		assertEquals("1984-05-15T14:30:04.124-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.1235-02:45")));
+
+		// missing colon in timezone
+		assertEquals("1984-05-15T14:30:04.123+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.123+01:30")));
+		assertEquals("1984-05-15T14:30:04.123-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14:30:04.123-0245")));
+
+		// dashes instead of colons in time
+		assertEquals("1984-05-15T14:30:04.123+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14-30-04.123+01:30")));
+		assertEquals("1984-05-15T14:30:04.123-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14-30-04.123-0245")));
+
+		// missing timezone
+		assertEquals("1984-05-15T14:30:04.123+00:00",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14-30-04.123")));
+
+		// missing milliseconds
+		assertEquals("1984-05-15T14:30:04.120+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14-30-04.12+01:30")));
+		assertEquals("1984-05-15T14:30:04.000-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-15T14-30-04-0245")));
+
+		// missing digits
+		assertEquals("1984-05-15T14:30:04.120+01:30",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-5-15T14-30-4.12+01:30")));
+		assertEquals("1984-05-05T14:30:04.000-02:45",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("1984-05-5T14-30-04-245")));
+		assertEquals("0984-05-05T14:30:04.000-02:05",
+				CalendarUtils.toISO8601(CalendarUtils
+						.fromISO8601("984-05-5T14-30-04-2:5")));
+
+		try {
+			CalendarUtils.fromISO8601("1984-05-15-14:30:04.123-02:45");
+			assertTrue(false);
+		} catch (IllegalArgumentException e) {
+		}
+	}
+
+	@Test
+	public void testFormat() throws ParseException {
+		String pattern = "yyyy-MM-dd HH:mm:ss Z";
+
+		Assert.assertEquals("2011-11-18 15:38:28 +0900", CalendarUtils.format(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28.0+09:00"),
+				pattern));
+		Assert.assertEquals("1984-05-15 14:30:00 +0900",
+				CalendarUtils.format(
+						CalendarUtils.fromISO8601("1984-05-15T14:30:00+09:00"),
+						pattern));
+	}
+
+	@Test
+	public void testGetTime() throws ParseException {
+		Assert.assertEquals(1321598308000l,
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")
+						.getTimeInMillis());
+		Assert.assertEquals(1321643308000l,
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28-03:30.003123")
+						.getTimeInMillis());
+	}
+
+	@Test
+	public void testGetTimeZone() throws ParseException {
+		Assert.assertEquals(TimeZone.getTimeZone("GMT+09:00"), CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00").getTimeZone());
+		Assert.assertEquals(TimeZone.getTimeZone("GMT-05:00"), CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28-05:00").getTimeZone());
+		Assert.assertEquals(TimeZone.getDefault(),
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28").getTimeZone());
+	}
+
+	@Test
+	public void testCompareTo() throws ParseException {
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00").compareTo(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) == 0);
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00").compareTo(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+08:00")) < 0);
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-17T15:38:28+09:00").compareTo(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) < 0);
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-19T15:38:28+09:00").compareTo(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) > 0);
+	}
+
+	@Test
+	public void testCompareToTimeZoneLess() throws ParseException {
+		Assert.assertTrue(CalendarUtils.compareToTimeZoneLess(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00"),
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) == 0);
+		Assert.assertTrue(CalendarUtils.compareToTimeZoneLess(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00"),
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+08:00")) == 0);
+		Assert.assertTrue(CalendarUtils.compareToTimeZoneLess(
+				CalendarUtils.fromISO8601("2011-11-17T15:38:28+09:00"),
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) < 0);
+		Assert.assertTrue(CalendarUtils.compareToTimeZoneLess(
+				CalendarUtils.fromISO8601("2011-11-19T15:38:28+09:00"),
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")) > 0);
+	}
+
+	@Test
+	public void testEquals() throws ParseException {
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00").equals(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")));
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00").equals(
+						CalendarUtils.fromISO8601("2011-11-18T14:38:28+08:00")));
+		Assert.assertFalse(CalendarUtils.fromISO8601(
+				"2011-11-19T15:38:28+09:00").equals(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")));
+	}
+
+	@Test
+	public void testAfter() throws ParseException {
+		Assert.assertFalse(CalendarUtils.fromISO8601(
+				"2011-11-18T15:38:28+09:00").after(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")));
+		Assert.assertFalse(CalendarUtils.fromISO8601(
+				"2011-11-18T15:38:28+09:00").after(
+				CalendarUtils.fromISO8601("2011-11-18T15:38:28+08:00")));
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-19T15:38:28+09:00").after(
+						CalendarUtils.fromISO8601("2011-11-18T15:38:28+09:00")));
+	}
+
+	@Test
+	public void testAddMilliseconds() throws ParseException {
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:33+09:00").compareTo(
+						CalendarUtils.addMilliseconds(CalendarUtils
+								.fromISO8601("2011-11-18T15:38:28+09:00"),
+								5000l)) == 0);
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:20+09:00").compareTo(
+						CalendarUtils.addMilliseconds(CalendarUtils
+								.fromISO8601("2011-11-18T15:38:28+08:00"),
+								-8000l)) == 0);
+		Assert.assertTrue(CalendarUtils
+				.fromISO8601("2011-11-18T15:38:20+09:00").compareTo(
+						CalendarUtils.addMilliseconds(CalendarUtils
+								.fromISO8601("2011-11-18T15:38:28+08:00"),
+								-8000l)) == 0);
+	}
+
+	@Test
+	public void testClone() throws ParseException {
+		Calendar original = CalendarUtils
+				.fromISO8601("2011-11-18T15:38:28+09:00");
+		Calendar clone = (Calendar) original.clone();
+		CalendarUtils.addMilliseconds(clone, 1000l);
+		Assert.assertFalse(original.getTimeInMillis() == clone
+				.getTimeInMillis());
+		Assert.assertTrue(original.getTimeInMillis() - clone.getTimeInMillis() == -1000);
+	}
+
+	@Test
 	public void testGetEarliestCalendar() {
-		assertEquals(cal1,
-				CalendarUtils.getEarliestCalendar(Arrays.asList(cal1)));
-		assertEquals(cal2,
-				CalendarUtils.getEarliestCalendar(Arrays.asList(cal2)));
-		assertEquals(cal3,
-				CalendarUtils.getEarliestCalendar(Arrays.asList(cal3)));
-		assertEquals(cal1, CalendarUtils.getEarliestCalendar(Arrays.asList(
-				cal1, cal2, cal3)));
-		assertEquals(cal1, CalendarUtils.getEarliestCalendar(Arrays.asList(
-				cal2, cal3, cal1)));
-		assertEquals(cal1, CalendarUtils.getEarliestCalendar(Arrays.asList(
-				cal3, cal1, cal2)));
+		assertEquals(this.cal1,
+				CalendarUtils.getEarliestCalendar(Arrays.asList(this.cal1)));
+		assertEquals(this.cal2,
+				CalendarUtils.getEarliestCalendar(Arrays.asList(this.cal2)));
+		assertEquals(this.cal3,
+				CalendarUtils.getEarliestCalendar(Arrays.asList(this.cal3)));
+		assertEquals(this.cal1, CalendarUtils.getEarliestCalendar(Arrays
+				.asList(this.cal1, this.cal2, this.cal3)));
+		assertEquals(this.cal1, CalendarUtils.getEarliestCalendar(Arrays
+				.asList(this.cal2, this.cal3, this.cal1)));
+		assertEquals(this.cal1, CalendarUtils.getEarliestCalendar(Arrays
+				.asList(this.cal3, this.cal1, this.cal2)));
 	}
 
 	@Test
 	public void testGetLatestCalendar() {
-		assertEquals(cal1, CalendarUtils.getLatestCalendar(Arrays.asList(cal1)));
-		assertEquals(cal2, CalendarUtils.getLatestCalendar(Arrays.asList(cal2)));
-		assertEquals(cal3, CalendarUtils.getLatestCalendar(Arrays.asList(cal3)));
-		assertEquals(cal3, CalendarUtils.getLatestCalendar(Arrays.asList(cal1,
-				cal2, cal3)));
-		assertEquals(cal3, CalendarUtils.getLatestCalendar(Arrays.asList(cal2,
-				cal3, cal1)));
-		assertEquals(cal3, CalendarUtils.getLatestCalendar(Arrays.asList(cal3,
-				cal1, cal2)));
+		assertEquals(this.cal1,
+				CalendarUtils.getLatestCalendar(Arrays.asList(this.cal1)));
+		assertEquals(this.cal2,
+				CalendarUtils.getLatestCalendar(Arrays.asList(this.cal2)));
+		assertEquals(this.cal3,
+				CalendarUtils.getLatestCalendar(Arrays.asList(this.cal3)));
+		assertEquals(this.cal3, CalendarUtils.getLatestCalendar(Arrays.asList(
+				this.cal1, this.cal2, this.cal3)));
+		assertEquals(this.cal3, CalendarUtils.getLatestCalendar(Arrays.asList(
+				this.cal2, this.cal3, this.cal1)));
+		assertEquals(this.cal3, CalendarUtils.getLatestCalendar(Arrays.asList(
+				this.cal3, this.cal1, this.cal2)));
 	}
 
 }
