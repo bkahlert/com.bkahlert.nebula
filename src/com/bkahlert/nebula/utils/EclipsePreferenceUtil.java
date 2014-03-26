5d4c5a6dc6ea203b446b6ff05f50728f1f004f5c
Björn Kahlert <mail@bkahlert.com>
2014-03-26 14:38:53 +0100

    [FEATURE] EclipsePreferenceUtil can now serve as a SourceProvider

--- src/com/bkahlert/nebula/utils/EclipsePreferenceUtil.java
+++ src/com/bkahlert/nebula/utils/EclipsePreferenceUtil.java
@@ -1,10 +1,16 @@
 package com.bkahlert.nebula.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.util.IPropertyChangeListener;
+import org.eclipse.ui.AbstractSourceProvider;
 import org.eclipse.ui.plugin.AbstractUIPlugin;
 
-public class EclipsePreferenceUtil {
+public class EclipsePreferenceUtil extends AbstractSourceProvider {
 
 	private final IPreferenceStore preferenceStore;
 
@@ -26,6 +32,29 @@ public class EclipsePreferenceUtil {
 
 	public IPreferenceStore getPreferenceStore() {
 		return this.preferenceStore;
+	}
+
+	@Override
+	final public String[] getProvidedSourceNames() {
+		Object[] sourceNameObjects = this.getCurrentState().keySet().toArray();
+		List<String> sourceNames = new ArrayList<String>(
+				sourceNameObjects.length);
+		for (Object sourceNameObject : sourceNameObjects) {
+			if (sourceNameObject != null) {
+				sourceNames.add(sourceNameObject.toString());
+			}
+		}
+		return sourceNames.toArray(new String[sourceNames.size()]);
+	}
+
+	@SuppressWarnings("rawtypes")
+	@Override
+	public Map getCurrentState() {
+		return new HashMap<String, Object>();
+	}
+
+	@Override
+	public void dispose() {
 	}
 
 }
