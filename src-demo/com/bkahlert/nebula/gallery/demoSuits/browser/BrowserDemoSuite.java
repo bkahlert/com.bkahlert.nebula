b71ff28f539983989ba7925732bf00c46ca05f22
Björn Kahlert <mail@bkahlert.com>
2014-05-26 00:59:57 +0200

    [INTERNAL] Removed JointjS Viewer

--- src-demo/com/bkahlert/nebula/gallery/demoSuits/browser/BrowserDemoSuite.java
+++ src-demo/com/bkahlert/nebula/gallery/demoSuits/browser/BrowserDemoSuite.java
@@ -6,14 +6,13 @@ import com.bkahlert.nebula.gallery.annotations.Demo;
 import com.bkahlert.nebula.gallery.demoExplorer.DemoSuite;
 import com.bkahlert.nebula.gallery.demoSuits.AbstractDemo;
 import com.bkahlert.nebula.gallery.demoSuits.browser.jointjs.JointJSDemo;
-import com.bkahlert.nebula.gallery.demoSuits.browser.jointjs.JointJSViewerDemo;
 import com.bkahlert.nebula.gallery.demoSuits.browser.timeline.TimelineDemoSuite;
 
 @DemoSuite(value = { BrowserDemo.class, MultipleBrowsersDemo.class,
 		ItemListDemo.class, ComposerDemo.class, ComposerReadOnlyDemo.class,
 		EditorDemo.class, MultipleEditorsDemo.class, ImageDemo.class,
 		TimelineDemoSuite.class, JQueryBrowserDemo.class,
-		BootstrapBrowserDemo.class, JointJSDemo.class, JointJSViewerDemo.class })
+		BootstrapBrowserDemo.class, JointJSDemo.class })
 @Demo
 public class BrowserDemoSuite extends AbstractDemo {
 
