fa98f61dd051637353590a744dd6ac6dc6e5f11a
Björn Kahlert <mail@bkahlert.com>
2014-06-06 02:49:38 +0200

    [FEATURE] New JointJS with Information demo

--- src-demo/com/bkahlert/nebula/gallery/demoSuits/browser/BrowserDemoSuite.java
+++ src-demo/com/bkahlert/nebula/gallery/demoSuits/browser/BrowserDemoSuite.java
@@ -6,13 +6,15 @@ import com.bkahlert.nebula.gallery.annotations.Demo;
 import com.bkahlert.nebula.gallery.demoExplorer.DemoSuite;
 import com.bkahlert.nebula.gallery.demoSuits.AbstractDemo;
 import com.bkahlert.nebula.gallery.demoSuits.browser.jointjs.JointJSDemo;
+import com.bkahlert.nebula.gallery.demoSuits.browser.jointjs.JointJSWithInformationDemo;
 import com.bkahlert.nebula.gallery.demoSuits.browser.timeline.TimelineDemoSuite;
 
 @DemoSuite(value = { BrowserDemo.class, MultipleBrowsersDemo.class,
 		ItemListDemo.class, ComposerDemo.class, ComposerReadOnlyDemo.class,
 		EditorDemo.class, MultipleEditorsDemo.class, ImageDemo.class,
 		TimelineDemoSuite.class, JQueryBrowserDemo.class,
-		BootstrapBrowserDemo.class, JointJSDemo.class })
+		BootstrapBrowserDemo.class, JointJSDemo.class,
+		JointJSWithInformationDemo.class })
 @Demo
 public class BrowserDemoSuite extends AbstractDemo {
 
