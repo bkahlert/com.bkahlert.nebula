da88a6769c26d274cfbad17828dffe45fc3ee22c
Björn Kahlert <mail@bkahlert.com>
2014-12-12 04:39:56 +0100

    [FEATURE] Multiple links connecting the same elements are now rendered so the links don't overlap

--- src/com/bkahlert/nebula/widgets/jointjs/html/js/facade.js
+++ src/com/bkahlert/nebula/widgets/jointjs/html/js/facade.js
@@ -56,6 +56,10 @@ com.bkahlert.nebula.jointjs = com.bkahlert.nebula.jointjs || {};
 			});
 			com.bkahlert.nebula.jointjs.setTitle(null);
 			
+			var myAdjustVertices = _.partial(adjustVertices, com.bkahlert.nebula.jointjs.graph);
+			com.bkahlert.nebula.jointjs.graph.on('add remove change:source change:target', myAdjustVertices);
+			com.bkahlert.nebula.jointjs.paper.on('cell:pointerup', myAdjustVertices);
+			
 			com.bkahlert.nebula.jointjs.activateZoomControls();
 			com.bkahlert.nebula.jointjs.activatePanCapability(com.bkahlert.nebula.jointjs.paper);
 			com.bkahlert.nebula.jointjs.activateLinkCreationCapability(com.bkahlert.nebula.jointjs.graph, com.bkahlert.nebula.jointjs.paper);
@@ -658,10 +662,13 @@ com.bkahlert.nebula.jointjs = com.bkahlert.nebula.jointjs || {};
 		
 		activateLinkCreationCapability: function(graph, paper) {
 			var shiftKey = false;
-			$(document).bind('keyup keydown', function(e){shiftKey = e.shiftKey || e.metaKey});
+			var altKey = false;
+			$(document).bind('keyup keydown', function(e){shiftKey = e.shiftKey || e.metaKey; altKey = e.altKey; });
 			
 			paper.on('cell:pointerdblclick', 
 				function(cellView, evt, x, y) {
+					if(!altKey) return;
+					
 					var bounds = { x: cellView.model.attributes.position.x, y: cellView.model.attributes.position.y, w: parseInt(cellView.model.attributes.size.width), h: parseInt(cellView.model.attributes.size.height) };
 					var id = cellView.model.id;
 					
@@ -1351,3 +1358,88 @@ joint.shapes.html.ElementView = joint.dia.ElementView.extend({
 	bounds.height = bounds.bottom - bounds.top;
 	return bounds;
 }})();
+
+
+// (c) http://jointjs.com/tutorial/multiple-links-between-elements
+function adjustVertices(graph, cell) {
+
+    // If the cell is a view, find its model.
+    cell = cell.model || cell;
+
+    if (cell instanceof joint.dia.Element) {
+
+        _.chain(graph.getConnectedLinks(cell)).groupBy(function(link) {
+            // the key of the group is the model id of the link's source or target, but not our cell id.
+            return _.omit([link.get('source').id, link.get('target').id], cell.id)[0];
+        }).each(function(group, key) {
+            // If the member of the group has both source and target model adjust vertices.
+            if (key !== 'undefined') adjustVertices(graph, _.first(group));
+        });
+
+        return;
+    }
+
+    // The cell is a link. Let's find its source and target models.
+    var srcId = cell.get('source').id || cell.previous('source').id;
+    var trgId = cell.get('target').id || cell.previous('target').id;
+
+    // If one of the ends is not a model, the link has no siblings.
+    if (!srcId || !trgId) return;
+
+    var siblings = _.filter(graph.getLinks(), function(sibling) {
+
+        var _srcId = sibling.get('source').id;
+        var _trgId = sibling.get('target').id;
+
+        return (_srcId === srcId && _trgId === trgId) || (_srcId === trgId && _trgId === srcId);
+    });
+
+    switch (siblings.length) {
+
+    case 0:
+        // The link was removed and had no siblings.
+        break;
+
+    case 1:
+        // There is only one link between the source and target. No vertices needed.
+        break;
+
+    default:
+
+        // There is more than one siblings. We need to create vertices.
+
+        // First of all we'll find the middle point of the link.
+        var srcCenter = graph.getCell(srcId).getBBox().center();
+        var trgCenter = graph.getCell(trgId).getBBox().center();
+        var midPoint = g.line(srcCenter, trgCenter).midpoint();
+
+        // Then find the angle it forms.
+        var theta = srcCenter.theta(trgCenter);
+
+        // This is the maximum distance between links
+        var gap = 20;
+
+        _.each(siblings, function(sibling, index) {
+
+            // We want the offset values to be calculated as follows 0, 20, 20, 40, 40, 60, 60 ..
+            var offset = gap * Math.ceil(index / 2);
+
+            // Now we need the vertices to be placed at points which are 'offset' pixels distant
+            // from the first link and forms a perpendicular angle to it. And as index goes up
+            // alternate left and right.
+            //
+            //  ^  odd indexes 
+            //  |
+            //  |---->  index 0 line (straight line between a source center and a target center.
+            //  |
+            //  v  even indexes
+            var sign = index % 2 ? 1 : -1;
+            var angle = g.toRad(theta + sign * 90);
+
+            // We found the vertex.
+            var vertex = g.point.fromPolar(offset, angle, midPoint);
+
+            sibling.set('vertices', [{ x: vertex.x, y: vertex.y }]);
+        });
+    }
+};
